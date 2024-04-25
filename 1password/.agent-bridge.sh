614aac94d80c8252138f4f775f290b882c3df3a9
Kyal Lanum <kyallanum@gmail.com>
2024-04-24 21:44:55 -0600

    Updated .agent-bridge.sh script

--- 1password/.agent-bridge.sh
+++ 1password/.agent-bridge.sh
@@ -1,29 +1,30 @@
 # Code extracted from https://stuartleeks.com/posts/wsl-ssh-key-forward-to-windows/ with minor modifications
 # Code further extracted from https://dev.to/d4vsanchez/use-1password-ssh-agent-in-wsl-2j6m with modifications to behavior.
 
-
 # Configure ssh forwarding
 export SSH_AUTH_SOCK=$HOME/.1password/agent.sock
-
 # need `ps -ww` to get non-truncated command for matching
 # use square brackets to generate a regex match for the process we want but that doesn't match the grep command running it!
-PIPE_ALREADY_RUNNING=$(ps -auxww | grep "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent" | tr -s " " | cut -d " " -f 2)
+# we use tr and cut to get the process id for each. If they are running then we restart them by killing them
 
-AGENT_ALREADY_RUNNING=$(ps -auxww | grep " ssh-agent$" | tr -s " " | cut -d " " -f 2)
+AGENT_RUNNING=$(ssh-add -l >/dev/null 2>&1; echo $?)
 
-if [[ -z "$AGENT_ALREADY_RUNNING" ]]; then
-    echo "Starting SSH Agent..."
-    eval "$(ssh-agent)" >> /dev/null
-fi
+if [[ $AGENT_RUNNING != "0" ]]; then
+    PIPE_PID=$(ps -axww | grep -q "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent" | tr -s " " | cut -d " " -f 2)
 
-if [[ -z "$PIPE_ALREADY_RUNNING" ]]; then
+    # We need to recreate the pipe. So lets get rid of the process and the file if they exist.
+    if [[ ! -z $PIPE_PID ]]; then
+        echo "Killing pipe process..."
+        kill -9 $PIPE_PID
+    fi
+
     if [[ -S $SSH_AUTH_SOCK ]]; then
-        # not expecting the socket to exist as the forwarding command isn't running (http://www.tldp.org/LDP/abs/html/fto.html)
         echo "Removing previous socket..."
         rm $SSH_AUTH_SOCK
     fi
+
     echo "Starting SSH-Agent relay..."
     # setsid to force new session to keep running
     # set socat to listen on $SSH_AUTH_SOCK and forward to npiperelay which then forwards to openssh-ssh-agent on windows
-    (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
+    (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &)
 fi
